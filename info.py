1d4af4ad33731fdddb6f6dab9166c6f1fc200373
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-11-29 15:09:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
-PORT = int(getenv('PORT', 8080))
+PORT = int(environ('PORT', 8080))
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
